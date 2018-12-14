c2e47d6e5b9613238cd887081f5274f2ccb901cd
Andy Lindsay <andydlindsay@gmail.com>
2018-12-14 14:40:21 -0800

    adds key up and down for wasd

--- gamepad.js
+++ gamepad.js
@@ -1,7 +1,7 @@
 // https://w3c.github.io/gamepad/
 
-var hasGP = false;
-var repGP;
+var hasGP = false; // has game pad
+var repGP; // report game pad (what game pad is doing)
 
 function canGame() {
     return "getGamepads" in navigator;
@@ -15,9 +15,9 @@ function reportOnGamepad() {
     if (gp.buttons[0].pressed) html += " pressed";
     html += "<br/>";
 
-    html += `Forward: ${gp.buttons[6].value.toFixed(3)} <br/>`;
+    html += `Forward: ${gp.buttons[7].value.toFixed(3)} <br/>`;
 
-    html += `Brake/Reverse: ${gp.buttons[7].value.toFixed(3)} <br/>`;
+    html += `Brake/Reverse: ${gp.buttons[6].value.toFixed(3)} <br/>`;
 
     html += `Turn: ${gp.axes[0].toFixed(3)}, ${gp.axes[1].toFixed(3)} <br/>`;
 
@@ -28,14 +28,72 @@ function reportOnGamepad() {
 
 $(document).ready(function () {
 
-    $(window).keypress(event => {
-        event.preventDefault();
+    let forward = 0; // 0.0 for off to 1.0 for on
+    let reverse = 0; // 0.0 for off to 1.0 for on
+    let turn = 0; // -1 for left to +1 for right
+    let cameraRotation = 0; // -1 for left to +1 for right
+
+    function output(key) {
+        console.clear();
+        console.log(`forward = ${forward}`);
+        console.log(`turn = ${turn}`);
+        console.log(`reverse = ${reverse}`);
+        console.log(`key == ${key}`);
+    }
+
+    const keys = [ '', 65, 87, 68, 83, 82 ];
+
+    $(window).keydown(event => {
         const key = event.which;
-        console.log(key);
-        if(key == 119) {
-            
+        // console.log(`key down == ${key}`);
+        if (keys.includes(key)) {
+            event.preventDefault();
+            switch (key) {
+                case 87: // w
+                    forward = 1;
+                    reverse = 0;
+                    break;
+                case 68: // d
+                    turn = 1; // right
+                    break;
+                case 83: // s
+                    forward = 0;
+                    reverse = 1;
+                    break;
+                case 82: // r
+                    cameraRotation = 0;
+                    break;
+                case 65: // a
+                    turn = -1; // left
+                    break;
+            }
         }
-    })
+        
+        output(key);
+    });
+
+    $(window).keyup(event => {
+        const key = event.which;
+        // console.log(`key = ${key}`);
+        if (keys.includes(key)) {
+            event.preventDefault();
+            switch (key) {
+                case 87: // w
+                    forward = 0;
+                    break;
+                case 65: // a
+                    turn = 0; // left
+                    break;
+                case 68: // d
+                    turn = 0; // right
+                    break;
+                case 83: // s
+                    reverse = 0;
+                    break;
+            }
+        }
+        output(key);
+    });
 
     if (canGame()) {
 
@@ -46,6 +104,7 @@ $(document).ready(function () {
             hasGP = true;
             $("#gamepadPrompt").html("Gamepad connected!");
             console.log("connection event");
+            // checking for control input
             repGP = window.setInterval(reportOnGamepad, 100);
         });
 
@@ -56,7 +115,7 @@ $(document).ready(function () {
             window.clearInterval(repGP);
         });
 
-        //setup an interval for Chrome
+        //setup an interval for Chrome to check if controller connected
         var checkGP = window.setInterval(function () {
             if (navigator.getGamepads()[0]) {
                 if (!hasGP) $(window).trigger("gamepadconnected");
