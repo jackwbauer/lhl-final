fe05a1ce682ffb177c99603b6ac96dd95c55ad87
Andy Lindsay <andydlindsay@gmail.com>
2018-12-18 01:36:29 +0000

    all of the lights

--- node-pi/server.js
+++ node-pi/server.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 const GPIO = require('onoff').Gpio;
 const LED_red = new GPIO(13, 'out');
 const LED_green = new GPIO(6, 'out');
-
+const LED_blur = new GPIO(5, 'out');
 
 const host = 'ws://rpi-lhl-final.herokuapp.com/';
 let ws = new WebSocket(host);
@@ -18,17 +18,22 @@ ws.on('close', () => {
 });
 
 ws.on('message', (data) => {
-    console.log(JSON.parse(data));
-    let direction = data.direction;
+   // console.log(JSON.parse(data));
+  const controls = JSON.parse(data);  
+  let direction = controls.direction;
+  console.log(direction);
     if(direction > 0) {
         LED_red.writeSync(0);
+      LED_blur.writeSync(1);
         LED_green.writeSync(1);
     } else if (direction < 0) {
         LED_green.writeSync(0);
+      LED_blur.writeSync(0);
         LED_red.writeSync(1);
     } else {
         LED_green.writeSync(0);
         LED_red.writeSync(0);
+      LED_blur.writeSync(1);
     }
 });
 
