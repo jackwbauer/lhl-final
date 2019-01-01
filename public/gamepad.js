8aa0a3a53a2b32de7c9a88d431da7bf055026d65
Andy Lindsay <andydlindsay@gmail.com>
2018-12-31 17:29:25 -0800

    removes console.log in gamepad.js

--- public/gamepad.js
+++ public/gamepad.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
     });
 
     socket.on('newDistance', (data) => {
-        console.log('distance data received');
+        // console.log('distance data received');
         $("#carDistance").text(`${Math.floor(data.distance)}cm to nearest obstruction`);
     });
 
